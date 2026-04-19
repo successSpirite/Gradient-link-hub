310b34c903f550df5e80278b8c5d886853029fcf
Asanyo Cletus <131569759+successSpirite@users.noreply.github.com>
2026-04-19 18:08:00 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+const section = document.querySelector('.section');
+const toggleSwitch = document.querySelectorAll('.cl-toggle-switch');
+
+toggleSwitch.forEach( tog=>{
+  tog.addEventListener('change', ()=>{
+  console.log("hi");
+  section.classList.toggle('dark-mode');
+
+
+});
+
+
+
+  if (section.classList.contains("dark-mode")) {
+    localStorage.setItem("sectionTheme", "dark");
+    
+  } else {
+    localStorage.setItem("sectionTheme", "light");
+  }
+});
+
+// Keep state after refresh
+window.addEventListener("load", () => {
+  if (localStorage.getItem("sectionTheme") === "dark") {
+    section.classList.add("dark-mode");
+    toggle.checked = true;
+  }
+});
+
+
+
+//scrollReveal
+ScrollReveal({ 
+    reset: false,      
+    distance: '35px',   // short + subtle for mobile
+    duration: 650,     
+    easing: 'ease-out',
+    mobile: true,
+    viewFactor: 0.2    
+  });
+
+  //  Hero image 
+  ScrollReveal().reveal('.photo', { 
+    delay: 100, 
+    origin: 'top',
+    distance: '20px',
+    scale: 0.98
+  });
+
+  //  "The Mrdanikings" text block
+  ScrollReveal().reveal('.writings', { 
+    delay: 250, 
+    origin: 'bottom'
+  });
+
+  //  All 7 service boxes 
+  
+  ScrollReveal().reveal('.box1-container, .box2-container', { 
+    delay: 400,       // wait until header is done
+    interval: 100,    // 100ms between each box = smooth wave
+    origin: 'bottom',
+    scale: 0.95       // tiny zoom-in makes it pop
+  });
+  
+
+  // Footer 
+  ScrollReveal().reveal('.media', { 
+    delay: 1200,      // comes in last
+    origin: 'bottom',
+    distance: '50px'
+  });
